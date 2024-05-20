01c86a555b47a0b85a2478b7af7ebf76d32b2971
DanielleFiene <daniellefiene@hotmail.com>
2024-05-20 22:53:50 +0200

    changed the entire thing

---
+++ process.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  
+  $to = 'daniellefiene@hotmail.com';
+
+  $subject = 'Contact Form Submission';
+
+  $headers = "From: $email" . "\r\n" .
+             "Reply-To: $email" . "\r\n" .
+             'X-Mailer: PHP/' . phpversion();
+
+  $message = 'Name: ' . $name . "\n" .
+             'Email: ' . $email . "\n" .
+             'Message: ' . $message;
+
+  mail($to, $subject, $message, $headers);
+  echo 'Thanks for contacting us!';
+}
+?>
